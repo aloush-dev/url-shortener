0ceebce93ff5e9e5a514994e412150f684d2238b
Ali <itsaliabdallah@gmail.com>
2023-07-09 17:03:19 +0100

    initial commit, remove uneeded files

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import LoginBox from "./components/LoginBox.vue";
+</script>
+
+<template>
+  <div id="homescreen">
+    <header>URL Shortener</header>
+
+    <main>
+      <LoginBox />
+    </main>
+  </div>
+</template>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 2rem;
+  background-color: antiquewhite;
+}
+</style>
---
+++ src/components/LoginBox.vue
@@ -0,0 +1,45 @@
+<script></script>
+
+<template>
+  <form>
+    <label>
+      email
+      <input />
+    </label>
+
+    <label
+      >password
+      <input />
+    </label>
+
+    <button>login</button>
+  </form>
+</template>
+
+<style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+label {
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+}
+
+input {
+  padding: 1rem;
+}
+
+button {
+  padding: 1rem;
+  margin: 1rem;
+  background-color: #00a1e4;
+  border: none;
+  color: white;
+  font-weight: 900;
+}
+</style>
